f488c66dcb68807a1903f6e1e789441a1d6cb0af
Lisa Huang <wenleh@andrew.cmu.edu>
2025-10-06 14:15:10 -0400

    added function and test for andrew ID question

--- utils/QueryProcessor.tsx
+++ utils/QueryProcessor.tsx
@@ -11,5 +11,11 @@ export default function QueryProcessor(query: string): string {
     return "Rohan";
   }
 
+  if (query.toLowerCase().includes("andrew id")) {
+    return (
+      "wenleh"
+    );
+  }
+
   return "";
 }
